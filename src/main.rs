aa741ab199979b8e0b4d78a2bf03f0ec2f7b478c
JiaFengYu <JiaFengYu@github.com>
2024-05-14 01:01:19 -0400

    Finished freestanding binary lecture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// since std lib requires os level features, we dont use it
 #![no_std]
+// we are not using main the standard entrypoint and providing 
+// our own, because the standrd one is os dependent
+#![no_main]
 
-fn main() {}
+
+use core::panic::PanicInfo;
+
+// current panic handler - can't do much so just loop
+// because we loop, so never return, therefore type !
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+// no mangle tells the compiler to NOT change the name of the fn
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+    loop {}
+}
