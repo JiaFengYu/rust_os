3c3904ff3ab88bea1e475e28dd7b9b9b4a25018d
JiaFengYu <JiaFengYu@github.com>
2024-05-19 22:35:45 -0400

    Finished module 2 minimal rust kernel!

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,21 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
 // no mangle tells the compiler to NOT change the name of the fn
 #[no_mangle]
 pub extern "C" fn _start() -> !{
+    // this is the entrypoint, and it is named _start 
+    // because the linker looks for _start as the entrypoint
+    
+    // why do we ned to cast it as a raw pointer?
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
