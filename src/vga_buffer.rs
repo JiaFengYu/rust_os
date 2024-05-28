ed44b1dbf7df42feb3dc3d6797ac5178403c9441
JiaFengYu <JiaFengYu@github.com>
2024-05-27 23:28:05 -0400

    Should be updated to default impl of testing from blog

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -157,7 +157,10 @@ use spin::Mutex;
 // is initialized when accessed for the first time (at runtime) 
 // instead of at compile time... blacc magicc??
 lazy_static! {
-    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+    // pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+    // pub isnt really necessary here because we are only calling 
+    // the WRITER object from within the file via print! and _print
+    static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
@@ -187,4 +190,28 @@ macro_rules! println {
 pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
+}
+
+
+// test suite:
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
